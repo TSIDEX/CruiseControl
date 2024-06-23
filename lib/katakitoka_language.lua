640f83b882e14a33fbdec01f029efbd8a9246373
TSIDEX <78680298+TSIDEX@users.noreply.github.com>
2024-06-23 22:05:52 +0500

    Add files via upload

---
+++ lib/katakitoka_language.lua
@@ -0,0 +1,21 @@
+local убшюушюушбуюшубюшбуюшуюуш = {
+    гбуюоугбоыюгубыобгюобыго = true,
+    юбуогбюугбогбюогубгобноыьбнобоукокб = 0,
+    бгушюобугюобугоыбугюоббо = renderCreateFont("Arial", 10, 9),
+    бшуюушбубшобуюбуобшюуобшуобшюуо = sampIsChatInputActive(),
+    уюбшуюшуюушбюубшушушбуошууюш = isSampfuncsConsoleActive(),
+    оуоуюбуоубубуюоубоубо = isKeyJustPressed,
+    шбуюшбуюбшуюшбуюушб = isKeyDown,
+    юбушюушбуюбшуюбшу = getCarSpeed,
+    шбушюгшбгуюшгюбшуюушг = storeCarCharIsInNoSave,
+    шгбюугшбошоубшгошбугогшуб = playerPed,
+    ьобпштоыьцопюцьшоюшьцтютш = isCharInAnyCar,
+    шбюшшбюушбюбшлшубюлушлу = getScreenResolution,
+    убюоуюбоубуоушбюшуюбшоуб = renderDrawBox,
+    ушбюшбуюшубошбюобшоубшуошбуошб = renderGetFontDrawTextLength,
+    бшюубюшубшюгшюгшыюшкюшгыбюкшы = renderFontDrawText,
+    бшуюшьюбпшьюшпьцюшцпбмюошцюопш = writeMemory,
+    убюшушюушбюушюушбуюушбюушубюш = wait,
+    }
+
+return убшюушюушбуюшубюшбуюшуюуш
